8edbeb66501f3f25a462f601e9588340d62f40b2
panchalnik <panchalnik285@gmail.com>
2026-05-01 13:47:25 +0530

    add them

---
+++ first.py
@@ -0,0 +1,18 @@
+a=10
+b=2
+c=a+b
+print(f"The sum of {a} and {b} is equal to {c}") 
+# here we are using string formattin by using f\F and {this contains values }# which is preventing us from using n commas
+print("The sum of a and b is" , c)
+# ""iske ander jo bhi aaega wo asitis print ho jeaga wo apka message hai
+prod=a*b
+print("the product of a and b is",prod)
+# divide=a//b (/ this simply divide and gives you exact value but // gives you integer value )
+# and modulus gives you the remainder
+# print(f"The result of {a} and {b} is {divide}")
+power=a**b
+print("The exponential value of a and b is ", power)
+# I/O operation 
+a=input("enter your number :")
+# here the a is a string type input  but if you want any number in it than you have to make it int.
+# a=int(input("enter your number :"))

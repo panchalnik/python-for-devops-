6542ad06632a6126648712a2cdee6005607ca2a3
panchalnik <panchalnik285@gmail.com>
2026-05-02 09:02:45 +0530

    add them

---
+++ second.py
@@ -0,0 +1,26 @@
+# type casting and type coversion
+# a=10
+# b=10.5
+# print(a+b)
+# float is greater than integer
+
+# num="100" ye abhi string hai aur coma hata kar int ban jaega
+# name="NIk"
+# print(num+" "+name)
+# print(type(name))
+# print(type(num))
+
+# typecasting
+# var1=float(100)
+# print(type(var1))
+# print(var1)
+
+# num1="50"
+# num2="100"
+# res=int(num1)+int(num2)
+# print(res)
+# res1=str(num1)+str(num2)
+# print(res1)
+# print(type(res1))
+# res2=float(num1)+float(num2)
+# print(res2)

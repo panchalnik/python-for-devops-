72f42e6517655183ab09c778300177c0a3046cb9
panchalnik <panchalnik285@gmail.com>
2026-05-02 16:40:12 +0530

    update them

---
+++ self_practice.py
@@ -0,0 +1,28 @@
+# print("hello nikki")
+# print(17*15)
+# print("Hey I am a good girl \n and this viewer is also a good person") 
+# # this is an escape sequence character (ek esa character jisko aap directly use nahi kr sakte )jo hamari statement ko nayi line se likhta hai 
+# print("I am going to be a successful person soon")
+# ''' triple single quote mai multiline comment daal sakte ho'''
+# print("Hey I am a \"good girl\" \n and this viewer is also a \"good person\"") 
+# # agar apko quotes ke ander quotes use krne hai apni statement mai then aap \" ko use karo usse apka particular term highlight ho jaega
+# print("hey",6,7,)
+# print("hey",6,7, sep"~", end= )
+# a="12"
+# b="52"
+# print(int(a)+int(b))
+# print(a+b)
+
+# system har input ko as a string leta hai unless ap usko defina na karo
+
+# a=input("enter your number")
+# b=input("enter your second number")
+# print(a+b)
+# print(int(a)+int(b))
+
+# you cannot put double quotes into double quotes it will give you error
+# triple single quotes ke ander aap multiple lines print karwa sakte wo as a string act karegi u can write anything in it.
+a='''he said, i am your friend
+and i live in ghaziabad that is just 20kms
+away from your location,so if u are free can we meet.'''
+print(a)

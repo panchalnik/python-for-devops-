874dba8f549ff2d8ad6a625b67443bcefd8e4dd2
panchalnik <panchalnik285@gmail.com>
2026-05-05 17:31:04 +0530

    update

--- claas.py
+++ claas.py
@@ -110,3 +110,68 @@ n2=40
 # res=n1==1 and n2==3 her we are using and operation similarly we can use or and not operation.
 # print(res)
 
+# 05/05/26
+# 5.Membership opr
+# str1="this id python for devops"
+# find="this"
+# print(find in str1)
+
+# we will study indexing start from 0,1,2,3, and -1 means aap backside se start karo
+# num1="4567"
+# print("4" not in num1)
+
+# email="iqindia123@gmail.com"
+# find="@gmail.com"
+# # print(find in email)    ye hamesha ek particular sequence mai check krega sequence gadbad false will be your output.
+# find1="@gmailiq"
+# print(find1 in email)
+# find2="i" and "3" idhar maine find2 ko 3 assign kiya kyuki ye comaparison nahi h simple value assign hai , hamesha updated value lega
+# print(find2 in email)
+# : ke baad se indent shuru hoga yani colon ki body
+
+email="iqindia123@gmail.com"
+if "@gmail.com" in email:
+    print("This is a  valid google mail id")
+else:
+    print("Invalid Email")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

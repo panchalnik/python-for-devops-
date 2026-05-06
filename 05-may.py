67276c3da67d785ba81c72fdec63522252ae3ea2
panchalnik <panchalnik285@gmail.com>
2026-05-06 09:13:18 +0530

    update

--- 05-may.py
+++ 05-may.py
@@ -3,8 +3,17 @@
 # str1="this id python for devops"
 # find="this"
 # print(find in str1)
+ 
+# str2="My brother is ghochu number 1"
+# find="My brother is love"
+# print(find in str2)
+# find1="My brother ghochu" sequence is important as is is missing therefore output is false
+# print(find1 in str2)
 
-# we will study indexing start from 0,1,2,3, and -1 means aap backside se start karo
+
+
+
+# we will study indexing start from 0,1,2,3, and -1 ,-2,-3,-4 means aap backside se start karo
 # num1="4567"
 # print("4" not in num1)
 
@@ -13,9 +22,17 @@
 # # print(find in email)    ye hamesha ek particular sequence mai check krega sequence gadbad false will be your output.
 # find1="@gmailiq"
 # print(find1 in email)
-# find2="i" and "3" idhar maine find2 ko 3 assign kiya kyuki ye comaparison nahi h simple value assign hai , hamesha updated value lega
+# find2="i" and "3" idhar maine find2 ko 3 assign kiya kyuki ye comaparison nahi h simple value assign hai , hamesha updated value
+# toh wo 3 ko find karega and updated value lega
 # print(find2 in email)
 # : ke baad se indent shuru hoga yani colon ki body
+
+
+email="iqindia123@gmail.com"
+find="i" and "3" and "4"
+print(find in email) 
+# kyuki mujhe 4 assign hui hai nayi value
+
 
 # email="iqindia123@gmail.com"
 # if "@gmail.com" in email:
@@ -24,15 +41,22 @@
 #     print("Invalid Email")
 
 # user is eligible for voting
-min_age=18
-nationality="IN"
-user_age=int(input("enter your age :"))
-user_nationality=input("enter your nationality :")
-if min_age <= user_age and user_nationality==nationality:
-    print("User is eigible for voting")
-else:
-    print("User is not eligible for voting")
+# min_age=18
+# nationality="IN"
+# user_age=int(input("enter your age :"))
+# user_nationality=input("enter your nationality :")
+# if min_age <= user_age and user_nationality==nationality:
+#     print("User is eigible for voting")
+# else:
+#     print("User is not eligible for voting")
 
 
     # practice
-    
+arun_age=20
+tarun_age=40
+if arun_age==tarun_age:  
+    print("They both are of same age")
+else:
+    print("They are not of equal age")
+
+

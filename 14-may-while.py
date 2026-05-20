cd2845caec6c15a7be34e5bff5cbe114e096cd03
panchalnik <panchalnik285@gmail.com>
2026-05-20 10:38:36 +0530

    update

---
+++ 14-may-while.py
@@ -0,0 +1,176 @@
+#while loop
+# initializer
+# condition 
+# increment/decrement
+
+
+# i=1
+# while i<=10:     #condition
+#     print(i)
+#     i+=1     #increment/decrement
+
+
+# i=1
+# while 10>=i:
+#     print(i)
+#     i-=1  #will give infinite values and crashes after sometime.
+
+# i=10
+# while i>=1:
+#     print(i)
+#     i-=1
+
+
+# wap to print only even number from 10-20
+# start=10 
+# end=20
+# while start<=end:
+#     if start%2==0:
+#         print(start)
+#     start+=1
+
+# i=2
+# while i<=20:
+#     if i%2==0:
+#         print(i)   
+#         i+=2 
+
+
+#Print numbers from 1 to 10 using while loop.
+# i=1
+# while i<=11:
+#     print(i)
+#     i+=1
+
+# Print numbers from 10 to 1 in reverse order.
+# i=10
+# while i>=1:
+#     print(i)
+#     i-=1
+
+
+# Print all even numbers between 1 and 20.
+# start=1
+# end=20
+# while start<=end:
+#    if start%2==0:
+#       print("The Even number is",start)
+#    start+=1   
+
+# Print all odd numbers between 1 and 20.
+# i=1
+# while i<=25:
+#     if i%2!=0:
+#         print(i,"is odd number")
+#         i+=2
+
+
+# Find the sum of numbers from 1 to 100.
+# a=1
+# sum=0
+# while a<=100:
+#     sum=sum+a
+#     a+=1
+#     print(sum)
+
+
+# a=int(input("Enter your number :"))
+# start=1
+# end=20
+# while start<=end:
+#     if start%2!=0:
+#         print("The odd number is ",start)
+#     start+=1
+
+# Find the factorial of a number using while loop.
+# while a>1:
+#     fact=fact*a
+#     print(fact)
+#     a-=1
+
+
+# Print the multiplication table of a number.
+# i=5
+# while i>0:
+#     print()
+
+# Count the digits in a number.
+
+# wap to print the total of even numbers from 1 to 15
+# i=1
+# sum=0
+# while i<=15:
+#     if i%2==0:
+#         print("The number is even",i)
+#         sum = sum+i
+#         print("The sum is:",sum)
+#     i=i+1
+        
+
+# wap to print whether number is palindrome or not        
+
+# text="madam"
+# copy_text=text
+# rev=""
+# i=len(text)-1
+# while i>=0:
+#     rev=rev+text[i]
+#     i-=1
+# if copy_text==rev:
+#     print("Palindrome")
+# else:
+#     print("Not a Palindrome")    
+
+
+# var1="DevOps Engineer"
+# copy_var1=var1
+# rev=""
+# i=len(var1)-1
+# while i>=0:
+#     rev=rev+var1[i]
+#     i-=1
+# if rev==copy_var1:
+#     print("Palindrome")
+# else:
+#     print("Not a Palindrome")
+
+# wap to check whether a number is palindrome or not
+
+# a=12321
+# abc=str(a)
+# i=len(abc)-1
+# rev=""
+# while i>=0:
+#     rev=rev+abc[i]
+#     i-=1
+# if rev==abc:
+#     print("Palindrome")
+# else:
+#     print("Not a Palindrome")        
+
+# wap to reverse the digits 1234 output should be 4321
+# a=1234
+# b=str(a)
+# rev=""
+# i=len(b)-1
+# while i>=0:
+#     rev=rev+b[i]
+#     i-=1
+#     print(rev)
+
+
+# a=1234
+# if a%10==0:
+#     print(a)    
+
+# wap to print formated table on a number given by user.
+a=int(input("Enter your number :"))
+n=1
+while n<=10:
+    m=a*n
+    print(axn=m)                                             #print(f"{a}x{n}={m}")
+    n+=1
+
+print("Your Table is Here 😊😊😊😊😊😊😊😊")   
+
+    
---
+++ 19-may-function.py
@@ -0,0 +1,214 @@
+# what is function in python.- 1.it means steps which are predefined and every function has their own purpose.
+# 2.A FUNCTION IS A BLOCK OF INSTRUCTION/CODE WHICH EXECUTE INSIDE ITS OWN BLOCK
+# 3. IT USES THE CONCEPT OF DRY MEANS DONT REPEAT YOURSELF.
+#4. FUNCTION HAS TWO MAIN PARTS FIRST FUNCTIONS DEFINATION SECOND IS FUNCTION CALLING.
+
+
+
+# HOW TO DEFINE FUNCTION IN PYTHON.
+# def add():                #  (parameter)
+#     a=10
+#     b=20
+#     c=a+b
+#     print(c)
+# add()                                  # (argument)
+
+# function is divided into four categories:
+# 1.tak nothing return nothing
+# 2.take nothing return something.
+# 3.take something return nothing.
+# 4.take something return something
+
+#parameter(para)and arguments (args)
+# and para =args alwzys
+#positional arguments
+# def add(a,b):
+#     c=a+b
+#     print(c)
+# add(10,20)
+# add(2,5)
+# add(8,15)
+# add(85,25)    #function apne aap ko 1000 bar call kar sakta hai.
+
+# write a function for formatting table    
+# def table_print(n):
+#     for i in range(1,11):
+#         print(f"{n} x{i} = {n*i}")
+# table_print(2) 
+# print("❌❌❌❌❌❌❌❌❌❌")       
+# table_print(5)
+# print(".........................")
+# table_print(10)
+
+
+# using while loop
+# def table_print(a):
+#     i=1
+#     while i<=10:
+#         print(f"{a} X {i} = {a*i}")
+#         i+=1
+# table_print(5)        
+
+# function banao aur usko call kar lo different outputs ke sath
+
+# def multiplication(a,b):
+#     print("multipication is ",a*b)
+# multiplication(10,5)
+# print(">>>>>>>>>>>>>>>>>>>")
+# multiplication(20,5)
+# print(".....................")
+
+# def division(a,b):
+#     print("division is :",a//b)          #using here floor division
+# division(15,3)
+# print("...............")
+# division(25,3)
+
+# def subtraction(a,b,c=0):
+#     print("subtraction is ",a-b)
+# subtraction(50,20)
+# print(">>>>>>>>>>>>>>>>>>") 
+# subtraction(20,50)   
+
+# alag tareeke se likho abs
+
+# def add(a,b):
+#     print("Addition is:", a+b)
+
+# def mul(a,b):
+#     print("multiplication is :",a*b)
+
+# def div(a,b):
+#     print("division is:",a//b)
+
+# def sub(a,b):
+#     print("subtraction is :", a-b)    
+
+# option=input("choose from : +,-,*,//:")    
+# if option=="+":
+#     add(10,20)
+# elif option=="//":
+#     div(20,10)    
+# elif option=="-":
+#     sub(20,10)
+# elif option=="*":
+#     mul(10,20) 
+
+# else:
+#     print("contact dev sir")           
+
+
+# IN PYTHON BY DEFAULT FUNCTION RETURN GIVES NONE.
+
+# def add(a,b):
+#     return "hello"                    
+#     return "djkaghiu"
+# res=add (2,3)
+# print(res)
+
+# def add(a,b):
+#     return a+b
+# res=add(10,20)
+# print(res)
+# add(10,20)
+# def sub(a,c):
+#     return a-c
+# print(sub(10,res))
+
+# function greet
+# def greet(a):
+#     return a
+# i=greet("Hello")
+# def user_name(a):
+#     return a
+# p=user_name("sudhir")
+# def greet(a):
+#     return ("Sudhir is paglet")
+# x=greet("Sudhir is paglet")
+# print(i,p)
+# print(x)
+
+# waf to check the function pass by arguments whether it is odd or even
+
+# def num(a):
+#     if a%2==0:
+#         print("The number is even:",a)
+#     else:
+#         print("The number is odd:",a)
+
+# num(3)            
+# num(2)
+# num(10)
+# num(5)
+
+# def num(a):
+#     pass
+# num(4)
+
+# waf to check which number is greater and two numbers pass by user.
+# def num(a,b):
+#     if a>b:
+#         print(a,":number a is greater")
+#     else:
+#         print(b,":number b is greater")    
+# num(10,20)        
+# num(50,40)
+
+# WAF to check a character pass by user is vowel or consonant
+# def char(a):
+#     if a=="a" or a=="e" or a=="i" or a=="o" or a=="u":
+#         print("vowel")
+#     else:
+#         print("consonant")
+# # a="x" 
+# a="i"     
+# char(a)                  
+
+# def char(a):
+#     if a in "aeiouAEIOU":
+#         print("vowel")
+#     else:
+#         print("consonant")
+# char("a")
+
+# NOW WE WILL return
+# WAF TO CHECK IS NUMBER COMPLETELY DIVIDED BY 2 AND 3 AND RETURN MESSAGE YES NUMBER IS COMPLETELY DIVISIBLE AND IF NOT THAN NO
+
+# def num(num):
+#     if num%2==0 and num%3==0:
+#         print(num,"Number is completely divisible")
+#     else:
+#         print(num,"Number is not divisible")
+
+# num(6)
+# num(9)
+# num(20)
+
+# def num(a):
+#     if a%2==0 and a%3==0:
+#         return"a ,divisible"
+#     else:
+#         return"a ,not divisible"
+# res=num(7)    
+# print(res)
+
+
+# waf to return length of a string pass by user without using length method.
+def len_string(a):
+    c=0
+    for i in a:
+        c+=1
+    return c    
+
+res=len_string("python")
+print(res)
+
+
+
+
+
+
+
+
+
+
